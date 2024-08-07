4464a35909ddac5f0468de3d0b57e0cf60445a7e
srsbandaru <86214783+srsbandaru@users.noreply.github.com>
2024-08-07 18:10:54 +1000

    Logo and Favicon fix
    Debug = False

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=+4&qj18!v+yld8nl)p^%g_q#e*3psb&4m@t%^ccx30d-u@b=^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","localhost"]
 
 
 # Application definition
